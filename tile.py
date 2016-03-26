8a976e8c75c97f0a4b56895a2968d204655d90e1
Elizabeth Williams <wizzah@gmail.com>
2016-03-26 00:39:33 -0400

    Remove comments

--- tile.py
+++ tile.py
@@ -4,8 +4,6 @@ from random import randint
 import argparse
 parser = argparse.ArgumentParser(description='Tiles a smaller image onto a larger one')
 parser.add_argument('big_image', type=str, help='Bigger image that gets tiled ontop of')
-# parser.add_argument('small_image', type=str, help='Smaller image that gets tiled ontop of the bigger image')
-# parser.add_argument('num_tiles', type=int, nargs='?', default=1, help='Default 1. Number of times to tile smaller image into bigger image')
 parser.add_argument('tile_attempts', type=int, nargs='?', default=50, help='Default 50. Number of times to attempt to tile smaller image into bigger image before giving up')
 parser.add_argument('--allow_overlap', dest='allow_overlap', action='store_true', help='Default False. Allows the smaller image to be placed ontop of previously placed images')
 parser.add_argument('small_image', type=str, nargs='+', help='Smaller images that get tiled ontop of the bigger image')
@@ -19,10 +17,7 @@ try:
 	big_im = Image.open(args.big_image)
 except IOError:
 	print "Could not open big image"
-# try:
-# 	small_im = Image.open(args.small_image)
-# except IOError:
-# 	print "Could not open small image"
+
 def rectangle_overlap(r1, r2):
 	'''
 	returns whether the two rectangles are overlapping
